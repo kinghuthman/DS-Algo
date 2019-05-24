8792187af992efd74c08ddee90cf169d29a546ef
Huthman King <kinghuthman@gmail.com>
2019-05-24 12:51:05 -0400

    Heaps - extractmax pop root, swap with the last node, sink that fake root down

--- DataStructures/Heaps/insertHeap.js
+++ DataStructures/Heaps/insertHeap.js
@@ -1,20 +1,19 @@
-/* Push the value into the values property on the heap.
-    Bubble the value up to its correct spot. 
-        Create a variable called index which is the length of the
-    values property - 1
-        Create a varirable called parentIndex which is the floor of
-    (index-1)/2
-        Keep looping as long as the values element at the parentIndex 
-    is less than the values element at the child index
-            Swap the value of the values element at the parentIndex
-        with the value of the element property at the child index
-            Set the index to be the parentIndex, and start over.
-*/
-
 class MaxBinaryHeap {
     constructor(){
         this.values = [];
     }
+    /* Push the value into the values property on the heap.
+        Bubble the value up to its correct spot. 
+            Create a variable called index which is the length of the
+        values property - 1
+            Create a varirable called parentIndex which is the floor of
+        (index-1)/2
+            Keep looping as long as the values element at the parentIndex 
+        is less than the values element at the child index
+                Swap the value of the values element at the parentIndex
+            with the value of the element property at the child index
+                Set the index to be the parentIndex, and start over.
+    */
     insert(element){
         this.values.push(element);
         this.bubbleUp();
@@ -46,4 +45,5 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
+heap.insert(49);
 console.log('list', heap);
